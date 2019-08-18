ce015657d3f51979623e82ef586a4a02c6a14cae
mamjerez <correomam@gmail.com>
2019-08-18 19:20:57 +0200

    Incluyo <script> en index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+// import { WebDataRocksPivot } from "./webdatarocks/webdatarocks.angular4"
 
 @Component({
   selector: 'app-root',
@@ -9,5 +9,4 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'WebDataRocksMAM';
-
 }
