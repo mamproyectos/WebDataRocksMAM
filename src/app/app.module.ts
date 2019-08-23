80793c1f5cd48bfee9e2516e736f36a780deec6a
mamjerez <correomam@gmail.com>
2019-08-23 19:17:06 +0200

    Funciona corectamente.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { WebDataRocksPivot } from './webdatarocks/webdatarocks.angular4';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WebDataRocksPivot
+    WebDataRocksPivot,
+    FooterComponent
   ],
   imports: [
     BrowserModule
