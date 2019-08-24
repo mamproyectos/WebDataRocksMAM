9e700a6bf3a79e4d68dd69444ee7f215439ec5a9
mamjerez <correomam@gmail.com>
2019-08-24 03:33:02 +0200

    Solo para avanzar rama UsoTS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import { WebDataRocksPivot } from "./webdatarocks/webdatarocks.angular4"
+import { WebDataRocksPivot } from "./webdatarocks/webdatarocks.angular4"
 
 @Component({
   selector: 'app-root',
